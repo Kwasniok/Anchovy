45962d5ae637260e7c0bca9c3fbea7e26dabb4fc
Kwasniok <Kwasniok@users.noreply.github.com>
2019-12-22 00:23:27 +0100

    added OverviewViewController

---
+++ Anchovy/OverviewViewController.h
@@ -0,0 +1,13 @@
+//
+//  OverviewViewController.h
+//  Anchovy
+//
+//  Created by Jens Kwasniok on 22.12.19.
+//  Copyright © 2019 Jens Kwasniok. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface OverviewViewController : NSViewController
+
+@end
---
+++ Anchovy/OverviewViewController.m
@@ -0,0 +1,22 @@
+//
+//  OverviewViewController.m
+//  Anchovy
+//
+//  Created by Jens Kwasniok on 22.12.19.
+//  Copyright © 2019 Jens Kwasniok. All rights reserved.
+//
+
+#import "OverviewViewController.h"
+
+@interface OverviewViewController ()
+
+@end
+
+@implementation OverviewViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+@end
