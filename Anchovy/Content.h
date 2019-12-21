4fe9c061a29cfde41a7b629c53e68e02c28e3dae
Kwasniok <Kwasniok@users.noreply.github.com>
2019-12-21 18:40:00 +0100

    added content

---
+++ Anchovy/Content.h
@@ -0,0 +1,13 @@
+//
+//  Content.h
+//  Anchovy
+//
+//  Created by Jens Kwasniok on 21.12.19.
+//  Copyright © 2019 Jens Kwasniok. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Content : NSObject
+
+@end
---
+++ Anchovy/Content.m
@@ -0,0 +1,13 @@
+//
+//  Content.m
+//  Anchovy
+//
+//  Created by Jens Kwasniok on 21.12.19.
+//  Copyright © 2019 Jens Kwasniok. All rights reserved.
+//
+
+#import "Content.h"
+
+@implementation Content
+
+@end
