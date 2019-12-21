bf38ad39d3e105e3daad547bc4d29643b59877b7
Kwasniok <Kwasniok@users.noreply.github.com>
2019-12-21 18:50:27 +0100

    added record

---
+++ Anchovy/Record.h
@@ -0,0 +1,13 @@
+//
+//  Record.h
+//  Anchovy
+//
+//  Created by Jens Kwasniok on 21.12.19.
+//  Copyright © 2019 Jens Kwasniok. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Record : NSObject
+
+@end
---
+++ Anchovy/Record.m
@@ -0,0 +1,13 @@
+//
+//  Record.m
+//  Anchovy
+//
+//  Created by Jens Kwasniok on 21.12.19.
+//  Copyright © 2019 Jens Kwasniok. All rights reserved.
+//
+
+#import "Record.h"
+
+@implementation Record
+
+@end
